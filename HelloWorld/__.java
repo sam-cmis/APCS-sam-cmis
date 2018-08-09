6755c8856dfec8a819c2d62f6dd6bfe65502e23b
Sam <samuel2329@cmis.ac.th>
2018-08-09 14:31:59 +0700

    Finish HelloWorld

---
+++ HelloWorld/__.java
@@ -0,0 +1,10 @@
+public class __
+    {
+    public static void main ( String args[] )
+        {
+        System.out.println( "Hello DOMINIC!" );
+        
+        } //end method main 
+        
+    } // end class HelloWorld
+    
