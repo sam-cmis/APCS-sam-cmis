54e1d87563a988f538d4ee276c8237980f5a92af
sam <samuel2329@cmsi.ac.th>
2018-08-14 09:35:08 +0700

    started on datatypes problem

---
+++ HelloWorld/Fundamentals1.java
@@ -0,0 +1,18 @@
+
+
+public class Fundamentals1
+{
+    public static void main (String args[] ){
+        dataTypes();
+        
+   }
+   
+   public static void dataTypes(){
+       System.out.println( "Byte: ");
+    }
+       
+
+       
+}
+
+    
--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,4 +1,4 @@
-public class __
+public class HelloWorld
     {
     public static void main ( String args[] )
         {
