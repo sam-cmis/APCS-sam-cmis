46fa4a49b894f04b36812dc33476e567e221dd58
sam <samuel2329@cmis.ac.th>
2018-08-15 11:56:52 +0700

    three examples of stuff

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -9,6 +9,20 @@ public class Fundamentals1
    
    public static void dataTypes(){
        System.out.println( "Byte: ");
+       byte a = 100;
+       short b = 10000;
+       int c = 100000;
+       long d = 100000L;
+       float e = 234.5f;
+       double f = 123.4;
+       boolean g = true;
+       char h = 'A';
+       String i = "A byte can store the number of toes, fingers, and discount value ";
+       String j = "A short can store the number of lego, most expensive lego brick, and the most expensive haircut";
+       String k = "A int can store the number of the price of lego minifigures, 
+    }
+    public static int add(int a, int b){
+        
     }
        
 
