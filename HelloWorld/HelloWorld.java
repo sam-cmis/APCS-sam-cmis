dbdb3f7d7b9cd27ce08fa228f028ac916fbc986c
sam <samuel2329@cmis.ac.th>
2018-08-16 14:36:33 +0700

    binary

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld
     public static void main ( String args[] )
         {
         System.out.println( "Hello DOMINIC!" );
-        
+        byte n =0b00101101;
+        System.out.println(n);
         } //end method main 
         
     } // end class HelloWorld
