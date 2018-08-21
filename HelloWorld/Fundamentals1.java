b9245c5d83f61a6468e2d68f770c5666e07fe440
sam <samuel2329@cmis.ac.th>
2018-08-21 12:05:18 +0700

    long

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -18,8 +18,10 @@ public class Fundamentals1
        boolean g = true;
        char h = 'A';
        String i = "A byte can store the number of toes, fingers, and discount value ";
-       String j = "A short can store the number of lego, most expensive lego brick, and the most expensive haircut";
-       String k = "A int can store the number of the price of lego minifigures, 
+       String j = "A short can store the number of lego, most expensive lego brick, and a haircut";
+       String k = "A int can store the number of the price of lego minifigures,a dog, and a laptop";
+       String l = "A long can store the number of the length of solar system in nanometers, biggest star we know in micrometers, mass of moon";
+       String m = "A float can store the 
     }
     public static int add(int a, int b){
         
