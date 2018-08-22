96b717df90d487175485d08886805048e98badcf
sam <samuel2329@cmis.ac.th>
2018-08-22 14:35:10 +0700

    finish

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -21,7 +21,11 @@ public class Fundamentals1
        String j = "A short can store the number of lego, most expensive lego brick, and a haircut";
        String k = "A int can store the number of the price of lego minifigures,a dog, and a laptop";
        String l = "A long can store the number of the length of solar system in nanometers, biggest star we know in micrometers, mass of moon";
-       String m = "A float can store the 
+       String m = "A float can store the length of 1000000 lightyears in cm, the milky way in nanometers, and the  biggest blackhole in milimeters";
+       String n = "A double can store the length of 100000000 lightyears in cm, the known universe in cm, and 1 quintillion Jeff Bezo's net worth";
+       String o = "A boolean can represent that every living thing will die eventually, life is suffering, and rocks are alive";
+       String p = "A char can store the number of the price of keyboards, price of mouse, and coffee";
+       System.out.format("Example data types: %n byte :%d%n short :%d%n int :%d%n long :%d%n float :%d%n double :%s boolean :%s char");
     }
     public static int add(int a, int b){
         
