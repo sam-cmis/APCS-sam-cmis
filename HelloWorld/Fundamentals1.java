18a2a9d36ae5e355b872e662e071ac580f0c8e72
sam <samuel2329@cmis.ac.th>
2018-08-24 09:55:13 +0700

    not yet

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -8,7 +8,6 @@ public class Fundamentals1
    }
    
    public static void dataTypes(){
-       System.out.println( "Byte: ");
        byte a = 100;
        short b = 10000;
        int c = 100000;
@@ -25,14 +24,50 @@ public class Fundamentals1
        String n = "A double can store the length of 100000000 lightyears in cm, the known universe in cm, and 1 quintillion Jeff Bezo's net worth";
        String o = "A boolean can represent that every living thing will die eventually, life is suffering, and rocks are alive";
        String p = "A char can store the number of the price of keyboards, price of mouse, and coffee";
-       System.out.format("Example data types: %n byte :%d%n short :%d%n int :%d%n long :%d%n float :%d%n double :%s boolean :%s char");
+       System.out.format("Example data types: %n byte :%d%n short :%d%n int :%d%n long :%d%n float :%f%n double :%f%n boolean :%s%n char :%s", a,b,c,d,e,f,g,h);
     }
-    public static int add(int a, int b){
-        
+   
+   public static double add(double a, double b){
+    double add = a + b;
+    return add;
+    }  
+   
+   public static double sub(double a, double b){
+    double sub = a - b;
+    return sub;
     }
-       
-
-       
+   
+   public static double mul(double a, double b){
+    double mul = a * b;
+    return mul;
+    }
+   
+   public static double div(double a, double b){
+    double div = a / b;
+    return div;
+    }
+   
+   public static int add(int a, int b){
+    int add = a + b;
+    return add;
+    }  
+   
+   public static int sub(int a, int b){
+    int sub = a - b;
+    return sub;
+    }
+   
+   public static int mul(int a, int b){
+    int mul = a * b;
+    return mul;
+    }
+   
+   public static int div(int a, int b){
+    int div = a / b;
+    return div;
+    }
+    
+   System.out.format("add :%d%n", );
 }
 
     
