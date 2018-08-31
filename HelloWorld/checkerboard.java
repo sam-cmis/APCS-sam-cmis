61248343bfd30d17de66435d470a1ff2cf2b73f0
sam <samuel2329@cmis.ac.th>
2018-08-31 12:00:07 +0700

    finally

--- HelloWorld/checkerboard.java
+++ HelloWorld/checkerboard.java
@@ -1,29 +1,40 @@
 import java.util.Scanner;
 public class checkerboard{
-  public static void main (String[] args){
-      Scanner Scan = new Scanner (System.in);
-      int in_1 = Scan.nextInt();
-      int in_2 = Scan.nextInt();
-      System.out.println(checkerboard(in_1, in_2));
-      
+    public static void main (String[] args){
+        Scanner Scan = new Scanner (System.in);
+        int in_1 = Scan.nextInt();
+        int in_2 = Scan.nextInt();
+        System.out.println(checkerboard(in_1, in_2));
+
     }
-      
+
     public static String checkerboard(int w, int h) {
-   String output = "";
-   String mid = "";
-   for (int a = 0; a <= h+1; a++){
-       if (a == 0){
-      for (int c = 0; a <= h + 1; a++){
-          mid += "-";
+        String output = "";
+        String mid1 = "";
+        String mid2 = "";
+        for (int a = 0; a <= h+1; a++){
+            if (a == 0){
+                for (int c = 0; c < w ; c++){
+                    mid1 += "-";
+                }
+                output += "+" + mid1 + "+";
+            }
+            else if (a == h +1){
+                for (int d = 0; d < w; d++){
+                  mid2 += "-";
+                }
+                output += "+" + mid2 + "+";
+            }
+            else if (a == 0){
+            
+            }
+            else{
+                for (int b = 0; b < w; b++){
+                    output += "."; 
+                }
+            }
+            output += "\n";
         }
-      output += "+" + mid + "+";
-      }else{
-     for (int b = 0; b < w; b++){
-            output += ","; 
-        }
+        return output;
     }
-    output += "\n";
-   }
-   return output;
-  }
 }
