5ad6cc285863f0681609996a5ec76ab3837f195a
E T H E R E A L <samuel@Durchhalten.local>
2018-09-02 17:47:17 +0700

    help me

--- HelloWorld/checkerboard.java
+++ HelloWorld/checkerboard.java
@@ -11,15 +11,38 @@ public class checkerboard{
     public static String checkerboard(int w, int h) {
    String output = "";
    String mid = "";
+   String mid2 = "";
    for (int a = 0; a <= h+1; a++){
        if (a == 0){
-      for (int c = 0; a <= h + 1; a++){
+      for (int c = 0; c <= w; a++){
           mid += "-";
         }
       output += "+" + mid + "+";
-      }else{
+      }
+      else if (a == h+1){
+        for (int d = 0; d < w; d++){
+         mid2 += "-";
+        }
+    }
+      else{
      for (int b = 0; b < w; b++){
             output += ","; 
+        }
+     if (b == 0 || b == w +1){
+        output += "|";
+        }
+     else if ( (b+a) % 2 == 0) {
+        while ((roll + column) % 2 != 0){
+          roll = (int)(Math.random() * ((h - 1) + 1)) + 1;
+          column = (int)(Math.random() * ((w - 1) + 1)) + 1;
+        }
+        if (a == roll && b == column){
+            output += num;
+        }else{
+            output += "#";
+        }
+        }else if ( (b+a) % 2 == 1){
+        output += " ";
         }
     }
     output += "\n";
