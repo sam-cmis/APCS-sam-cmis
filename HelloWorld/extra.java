37a6f0be0d94cd7742b5ecdfa832b15d788790d3
sam <samuel2329@cmis.ac.th>
2018-09-06 11:55:51 +0700

    no

--- HelloWorld/extra.java
+++ HelloWorld/extra.java
@@ -1,23 +1,24 @@
 else{
-                for (int b = 0; b < w; b++){
-                    output += ","; 
-                }
-                if (b == 0 || b == w +1){
-                    output += "|";
-                }
-                else if ( (b+a) % 2 == 0) {
-                    while ((roll + column) % 2 != 0){
-                        roll = (int)(Math.random() * ((h - 1) + 1)) + 1;
-                        column = (int)(Math.random() * ((w - 1) + 1)) + 1;
-                    }
-                    if (a == roll && b == column){
-                        output += num;
-                    }else{
-                        output += "#";
-                    }
-                }else if ( (b+a) % 2 == 1){
-                    output += " ";
-                }
+for (int b = 0; b < w; b++){
+output += ","; 
+}
+if (b == 0 || b == w +1){
+output += "|";
+}
+else if ( (b+a) % 2 == 0) {
+while ((roll + column) % 2 != 0){
+roll = (int)(Math.random() * ((h - 1) + 1)) + 1;
+column = (int)(Math.random() * ((w - 1) + 1)) + 1;
+}
+if (a == roll && b == column){
+output += num;
+}else{
+output += "#";
+}
+}else if ( (b+a) % 2 == 1){
+output += " ";
+}
+
                 
                 
                 
@@ -26,24 +27,14 @@ else{
                 
                 
                 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                for (int lup = h; h>0; h--)
-            {
-             System.out.print("|");
-             for (int looptime = w; w > 0; w--)
-             {
-                System.out.print(".");
-                if (w==1){
-                 System.out.print("| \n");
-                }
-                }
-            }
+for (int lup = h; h>0; h--)
+{
+System.out.print("|");
+for (int looptime = w; w > 0; w--)
+{
+System.out.print(".");
+if (w==1){
+System.out.print("| \n");
+}
+}
+}
