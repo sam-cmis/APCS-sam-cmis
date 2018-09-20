0bc8f13f281eb2b0d5bf5e30f172c8452bb58db1
sam <samuel2329@cmis.ac.th>
2018-09-20 14:40:42 +0700

    ok

---
+++ HelloWorld/Fundamentals2.java
@@ -0,0 +1,14 @@
+public class Fundamentals2{
+    public static void main (String args[]){
+        int [] itg = {21, 69, 74, 42, 1};
+        double [] dbl = {1,2,3,4,5};
+        String [] str = {"Eggs Benedict", "womble", "soviet", "mamamia", "over"};
+        System.out.println(itg[0]);
+        System.out.println(dbl[2]);
+        System.out.println(str[4]);
+    }
+    public static void printArray(type array, boolean skip){
+    
+    }
+
+}
