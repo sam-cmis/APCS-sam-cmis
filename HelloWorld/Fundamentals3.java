5606e246f30fab02bc3a980bf9660c3f857a3aed
sam <samuel2329@cmis.ac.th>
2018-10-30 14:36:57 +0700

    cloverfield

--- HelloWorld/Fundamentals3.java
+++ HelloWorld/Fundamentals3.java
@@ -1,17 +1,32 @@
 public class Fundamentals3 {
     public static void main(String[] args){
-        defaultArray(5,5);
+        int[][] array_1 = new int[5][5];
+        int[][] array_2 ={{4,9,1,9,2},{4,5,6,7,3},
+                {1,5,3,9,2},{5,3,4,6,9},{6,9,7,4,3}};
+        double[][] array_3 = new double[4][4];
+        double[][] array_4 = {{4,5,6,7,8},{5,3,2,4,5,},{7,8,9,1,2,},{1,2,3,4,5,},{3,4,5,6,7,}};
+        String[][] please = {{" ","#","#","#"," ","#"," ","#"},{" ","#","#","#"," ","#"," ","#"}
+            ,{" ","#","#","#"," ","#"," ","#"},{" ","#","#","#"," ","#"," ","#"},{" ","#","#","#"," ","#"," ","#"}
+            ,{" ","#","#","#"," ","#"," ","#"},{" ","#","#","#"," ","#"," ","#"},{" ","#","#","#"," ","#"," ","#"}};
     }
-    public static int[][] defaultArray (int h, int w){
-        int[][] array = new int[h][w];
-        int z = 0;
-        for (int y = 0; y < array.length; y++){
-            for (int x = 0; x < array[0].length; x++){
-                z++; 
-                System.out.print(z + " "); 
-            }
-            System.out.println(); 
-        }
-        return array;
+    
+
+    public static int ifaequalschoiceloopit (int array[][], int r, int c){
+        int i = array[r][c];
+        return i;
+    }
+    
+    public static double ifaequalschoiceloopit (double array[][], int r, int c){
+        double i = array[r][c];
+        return i;
+    }
+    
+    public static String ifaequalschoiceloopit (String array[][], int r, int c){
+        String i = array[r][c];
+        return i;
+    }
+    
+    public static void setValue(int[][] arr, int r, int c, int v){
+
     }
 }
