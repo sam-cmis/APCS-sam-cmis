746409b7baa0ad92886fedf9263b135e437c6c61
sam <samuel2329@cmis.ac.th>
2019-01-28 11:55:02 +0700

    mon

--- HelloWorld/MergeSort.java
+++ HelloWorld/MergeSort.java
@@ -20,33 +20,50 @@ public class MergeSort
     public static int sort(int[] array){
         int[] one = new int[array.length/2];
         int[] second = new int[array.length - array.length/2];
-        int frontarray = 0;
-        int backarray = 0;
         int a = 0;
         int b = 0;
-        if (array[one] < array[second]){
-            for (int x = 0; x < array.length; x++){
-                one[x] = second[x];
-                for (int y = 1; y< array[0].length; y++){
-                    second[x] = one[a];
-                    a++;
-                    if(array[y] < array[z])
-                    {
-                        z = y;
+        while (b < array.length){
+            if (one[a] < second[b]){
+                for (int x = 0; x < array[second].length; x++){
+                    one[x] = second[x];
+                    for (int y = 0; y< array[0].length; y++){
+                        second[x] = one[a];
+                        a++;
+                        if(array[y] < array[z])
+                        {
+                            z = y;
+                        }
+                        int a = array[x];
+                        array[x] = array[z];
+                        array[z] = a;
                     }
-                    int a = array[x];
-                    array[x] = array[z];
-                    array[z] = a;
                 }
             }
-        }
+            else if (one ){
+                for (int x = 0; x < array[one].length; x++){
+                    second[x] = one[x];
+                    for (int y = 1; y< array[0].length; y++){
+                        one[a] = second[x];
+                        x++;
+                        if(array[z] < array[y])
+                        {
+                            y = z;
+                        }
+                        int b = array[y];
+                        array[y] = array[z];
+                        array[z] = b;
+                    }
+                }
+            }
 
-        else if(array.length == 0 || 1){
-            return array;
+            else if (array.length == 0){
+                return 0;
+            }
+            else if(array.length == 1){
+                return array;
+            }
+            }
+        return array;
         }
-        else {
-            return sort(
-
-        }
+    //increment sides using the index that is being tracked.
     }
-}
