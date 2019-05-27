6e826269d2fbed37600f183d0d31ce6b7f08e621
S <s>
2019-05-27 11:43:44 +0700

    S

--- Interfaces/Final Project/Genghis.java
+++ Interfaces/Final Project/Genghis.java
@@ -11,7 +11,7 @@ public class Genghis extends Actor
     private final int GRAVITY = 1;
     private int velocity;
     public Genghis(){
-        velocity = 5;
+        velocity = 0;
     }
     /**
      * Act - do whatever the Character wants to do. This method is called whenever
@@ -21,34 +21,36 @@ public class Genghis extends Actor
     {
         // Add your action code here.
         fall();
-        if (Greenfoot.isKeyDown("w") && getY() > getWorld().getHeight() - 50)jump();
+        if (Greenfoot.isKeyDown("w") && getY() > getWorld().getHeight() - 70)jump();
         
          if (Greenfoot.isKeyDown("d")){
             move(1);
-            if (isTouching(platform.class)){
+            if (isTouching(platform.class))
+            {
                 move(-1);
             }
         }
         if (Greenfoot.isKeyDown("a")){
             move(-1);
-            if (isTouching(platform.class)){
+            if (isTouching(platform.class))
+            {
                 move(1);
             }
         }
-        if (Greenfoot.isKeyDown("a")){
-            move(-1);
-            if (isTouching(platform.class)){
-                move(1);
-            }
-        }
+        
     }
     public void fall(){
         setLocation(getX(), getY() + velocity);
-        if (getY() > getWorld().getHeight() - 50)velocity = 0;
+        if (getY() > getWorld().getHeight() - 70)
+        {
+            velocity = 0;
+        }
         else velocity += GRAVITY;
+        
     }
     public void jump(){
-        velocity = -10;
+        velocity = -25;
     }
+    
     //if a key is pressed, shoot a projectile named horse
 }
